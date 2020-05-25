88f22b74c46eb4028571a39cef23c228ecae6e93
Juan Pedro Villa Isaza <584947+jpvillaisaza@users.noreply.github.com>
2020-05-25 08:28:17 -0500

    Add error to auth helper

--- src/Web/Pocket.hs
+++ src/Web/Pocket.hs
@@ -28,31 +28,52 @@ import Web.Pocket.Send
 import qualified Data.Text as Text
 
 
-authReq :: (MonadIO m, MonadThrow m) => AuthRequest -> m (Either Error AuthResponse)
+authReq
+  :: (MonadIO m, MonadThrow m)
+  => AuthRequest
+  -> m (Either Error AuthResponse)
 authReq =
   request "POST https://getpocket.com/v3/oauth/request"
 
-authGet :: AuthRequest -> IO String
+authGet
+  :: (MonadIO m, MonadThrow m)
+  => AuthRequest
+  -> m (Either Error String)
 authGet ar = do
-  Right authRsp <- authReq ar
-  pure $
-    "https://getpocket.com/auth/authorize?request_token="
-      <> Text.unpack (authRespCode authRsp)
-      <> "&redirect_uri="
-      <> Text.unpack (authReqRedirectUri ar)
+  eAuthRsp <- authReq ar
+  case eAuthRsp of
+    Left e -> pure (Left e)
+    Right authRsp ->
+      pure $ Right $
+        "https://getpocket.com/auth/authorize?request_token="
+          <> Text.unpack (authRespCode authRsp)
+          <> "&redirect_uri="
+          <> Text.unpack (authReqRedirectUri ar)
 
-authorizeReq :: (MonadIO m, MonadThrow m) => AuthorizeRequest -> m (Either Error AuthorizeResponse)
+authorizeReq
+  :: (MonadIO m, MonadThrow m)
+  => AuthorizeRequest
+  -> m (Either Error AuthorizeResponse)
 authorizeReq =
   request "POST https://getpocket.com/v3/oauth/authorize"
 
-add :: (MonadIO m, MonadThrow m) => AddRequest -> m (Either Error AddResponse)
+add
+  :: (MonadIO m, MonadThrow m)
+  => AddRequest
+  -> m (Either Error AddResponse)
 add =
   request "POST https://getpocket.com/v3/add"
 
-get :: (MonadIO m, MonadThrow m) => GetRequest -> m (Either Error GetResponse)
+get
+  :: (MonadIO m, MonadThrow m)
+  => GetRequest
+  -> m (Either Error GetResponse)
 get =
   request "POST https://getpocket.com/v3/get"
 
-send :: (MonadIO m, MonadThrow m) => SendRequest -> m (Either Error SendResponse)
+send
+  :: (MonadIO m, MonadThrow m)
+  => SendRequest
+  -> m (Either Error SendResponse)
 send =
   request "POST https://getpocket.com/v3/send"
